f9d5a933ec29411f73bd78d344719dd4bffe4713
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2021-12-18 09:30:14 -0800

    Final version

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Module3-Challange
 password generator project
+In this project, my task was to create a JS script so that can create a password
+
+This first thing that i did was create an propmt that asked for the length of the password. This was initialized in the writePassword() function.
+This would allow the user to put any number between 8 and 128 characters. If there is a letter or special character, the program would ask the user
+to input a number between 8 and 128 again.
+
+Once the user inputs a legal number, the program then moves to ask the user if they want letters in their password, if they select ok, then letters will be initiated.
+The same goes for Number and special characters
+
+The user can pick any conbination of these options 
+
+The possible characters and then put into a for loop that randomizes the possible characters until the length of the password has been met.
+
+After this, I put in a passValidate() to ensure that at least one of each character is in the password. This is mostly for small password that have a lot of options, i.e. 8 character passwords that have all he inputs. So that should it not have a numbe when numbers are selected, it will go back into the generatePassword() function until the critaria is met.
+
+Once a valid password is created, it will print it out to the text area.
+
+
+Note:
+I just wanted to add that, before i got clarification about the project, i created a cool function called letters() that would prompt the user to include a string, validate that it would only be uppercase or lowercase letters, the use that to create a password. I was going to do the same to numbers and special characters. I wanted to add this because i think i spent more time creating that function then the entire time it took to create the project.
+
+I included it here to give the js code a cleaner look, but i couldn't bring myself to delete it
+
+function letters() {
+  var Arraychars = [window.prompt("Please pick the letters you want in your password")];
+  chars = Arraychars.toString();
+  chars = chars.replace(/[ ,]/g , "");
+  if(!/^[a-zA-z]+$/.test(chars)){
+    window.alert("Letters only please");
+    letters();
+  }
+  else {
+    return chars;
+  }
+  return chars;  
+}
--- script.js
+++ script.js
@@ -7,43 +7,27 @@ var charLet= "";
 var charNum = "";
 
 
-
-console.log(numLength);
-
-
 //this function is to return a valid number for the password length
 function numValidate(){
   numLength = parseInt(window.prompt("pick a password length between 8 and 128"));
-  if ( /^[0-9,.]+$/.test(numLength)) {
+  if ( /^[(0-9,.)]+$/.test(numLength)) {
     console.log('pass');
     if(numLength>=8 && numLength <=128){
-      window.alert("great choice on a " + numLength + " long password");
+      window.alert("great choice on a " + numLength + " character long password");
     }
     else{
-      window.alert("lets try that again...");
+      window.alert("lets try that again...Numbers between 8 and 128");
       numValidate();
     }
     return numLength;
 
   } else {
-  window.alert("lets try that again..");
+  window.alert("lets try that again...Numbers between 8 and 128");
   numValidate();
   }
 }
 
-function letters() {
-  var Arraychars = [window.prompt("Please pick the letters you want in your password")];
-  chars = Arraychars.toString();
-  chars = chars.replace(/[ ,]/g , "");
-  if(!/^[a-zA-z]+$/.test(chars)){
-    window.alert("Letters only please");
-    letters();
-  }
-  else return chars;
-  console.log(chars);
-  return chars;  
-}
-
+//this function confirm if the user wanted to include letters in their password
 function PLetter(){
 charLet = window.confirm("do you want letters?");
 
@@ -58,21 +42,21 @@ charLet = window.confirm("do you want letters?");
   return charLet;
 }
 
+//this function confirms if the user wants to include numbers in their password
 function PNumbers(){
   charNum = window.confirm("do you want numbers?");
 
   if (charNum){
 
     charNum = "1234567890";
-
   }
   else{
-    charNum = "";
-    
+    charNum = "";  
   }
   return charNum;
 }
 
+//this function confirms if the user wants to include special characters in their password
 function PspChars(){
   spChars = window.confirm("Do you want to include special characters?");
   if (spChars){
@@ -84,15 +68,27 @@ function PspChars(){
   return spChars;
 }
 
+//this is a validation function that test to see if the generated password includes at least one of each criteria 
+function passValidate(){
+  if (/^[a-zA-z0-9~!@#$%^&*(){}|]+$/.test(password)){
+    console.log("yup");
+    return password;
+  }
+  else{
+    console.log("nope");
+    generatePassword();
+  }
+}
+
 
 function generatePassword(){
-  numValidate();
-  //letters();
   PLetter();
   PNumbers();
   PspChars();
-    //var chars = "abcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*(){}|ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  //In this area, i included a randomizer that adds all the possible characters and randomizes them to the length the user chose
     chars = charNum + charLet+spChars;
+    var password="";
   for( var i = 0; i <= numLength; i++){
     var randomNum = Math.floor(Math.random()* chars.length);
     password += chars.substring(randomNum, randomNum + 1);
@@ -102,15 +98,16 @@ function generatePassword(){
 }
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();  
+  numValidate();
+  password = generatePassword();  
+  console.log(password);
+  passValidate();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-
-  
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
